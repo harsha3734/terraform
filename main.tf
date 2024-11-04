ce28a6bdd44f49264eadd09147dc80345800a365
harsha3734 <harsha.reddy3734@gmail.com>
2024-11-04 07:46:10 -0600

    main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,4 @@
-variable "old_instance_id" {
-  description = "ID of the old instance to copy details from and detach resources"
-  type        = string
-}
-
-# Fetch details of the old EC2 instance
+# Fetch details of the old instance using its ID
 data "aws_instance" "old_instance" {
   instance_id = var.old_instance_id
 }
@@ -13,7 +8,7 @@ data "aws_eip" "old_eip" {
   instance = var.old_instance_id
 }
 
-# Create a new EC2 instance with the same network configuration as the old instance
+# Create a new instance using the same AMI, subnet, and security groups from the old instance
 resource "aws_instance" "new_instance" {
   ami                         = data.aws_instance.old_instance.ami
   instance_type               = data.aws_instance.old_instance.instance_type
@@ -22,19 +17,30 @@ resource "aws_instance" "new_instance" {
   associate_public_ip_address = data.aws_instance.old_instance.associate_public_ip_address
 
   tags = {
-    Name = "NewInstance"
+    Name = "NewInstanceFromOldInstance"
   }
 }
 
 
 
-# Detach each EBS volume from the old instance
+# Detach EBS volumes from the old instance
 resource "aws_volume_attachment" "detach_ebs_volumes" {
   for_each    = { for vol in data.aws_instance.old_instance.ebs_block_device : vol.volume_id => vol }
   device_name = each.value.device_name
   volume_id   = each.value.volume_id
   instance_id = var.old_instance_id
   force_detach = true
+
+}
+
+# Attach EBS volumes to the new instance
+resource "aws_volume_attachment" "attach_ebs_volumes" {
+  for_each    = { for vol in data.aws_instance.old_instance.ebs_block_device : vol.volume_id => vol }
+  device_name = each.value.device_name
+  volume_id   = each.value.volume_id
+  instance_id = aws_instance.new_instance.id
+
+  depends_on = [aws_volume_attachment.detach_ebs_volumes]
 }
 
 # Detach the Elastic IP from the old instance
@@ -48,18 +54,10 @@ resource "aws_eip_association" "detach_eip" {
 
 }
 
-# Attach each detached EBS volume to the new instance
-resource "aws_volume_attachment" "attach_ebs_volumes" {
-  for_each    = { for vol in data.aws_instance.old_instance.ebs_block_device : vol.volume_id => vol }
-  device_name = each.value.device_name
-  volume_id   = each.value.volume_id
-  instance_id = aws_instance.new_instance.id
-  depends_on = [aws_volume_attachment.detach_ebs_volumes]
-}
-
 # Attach the Elastic IP to the new instance
 resource "aws_eip_association" "attach_eip" {
   instance_id   = aws_instance.new_instance.id
   allocation_id = data.aws_eip.old_eip.id
-  depends_on    = [aws_eip_association.detach_eip]
+
+  depends_on = [aws_eip_association.detach_eip]
 }
